4b59a5aa301988ef908e72d5d73fd36eacf3db7d
Chuan Jiang <jiangchuan.mail@gmail.com>
2025-01-01 01:52:06 +0800

    Fix various problems on latest dependencies

--- setup.py
+++ setup.py
@@ -4,20 +4,22 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="piano-transcription-inference", # Replace with your own username
+    name="piano-transcription-inference",
     version="0.0.5",
     author="Qiuqiang Kong",
     author_email="qiuqiangkong@gmail.com",
+    maintainer="Chuan Jiang",
+    maintainer_email="jiangchuan.mail@gmail.com",
     description="Piano transcription inference toolbox",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/jc-stu/piano_transcription_inference",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['matplotlib', 'mido', 'librosa', 'torchlibrosa'],
+    install_requires=['matplotlib', 'mido', 'librosa', 'torchlibrosa', 'resampy'],  # resampy for librosa
     python_requires='>=3.6',
 )
